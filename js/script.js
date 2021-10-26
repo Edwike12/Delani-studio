2471da48c9fc6e37023f0624fb6ea35ed0f502a5
Edwike12 <nyaunchoedwike@gmail.com>
2021-10-26 23:46:06 +0300

    commit all changes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 
-let text = '';
-console.log("'' we have received your message.Thank you for contacting us.")
+const Name = {
+    Name: function() {
+      return this.Name + " " + this.Message;
+    }
+  }
+  const Email = {
+    Email =""
+  }
+  const Message = {
+    Message:"  we have received your message.Thank you for contacting us",
+  }
+   
+  document.getElementById("demo").innerHTML = form.Name.call(); 
+
